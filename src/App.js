fa03346ee17eeeff61642f04dfa00500333d8bf6
Rachele Edwards <dot.com1472@gmail.com>
2020-05-08 10:54:55 -0600

    able to set up app and show the data from my projects api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+const [projList, setProjList] = useState([])
+const [project, setProject] = useState({
+  name: "",
+  description: "",
+  complete: false
+})
+
+  useEffect(() => {
+    axios.get('https://projects-list-app.herokuapp.com/api/projects')
+    .then(res => {
+      console.log({res})
+      setProjList(res.data)
+    })
+    .catch(err => {
+      console.log({err})
+    })
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Projects</h1>
+        <div>
+          {projList && projList.map(showProj => {
+            return(
+              <div>
+                <p>Name: <br/>{showProj.name}</p><br/>
+                <p>Description: <br/>{showProj.description}</p>
+              </div>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
